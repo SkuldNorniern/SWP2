2c05c57c768aa8208f61fdda800b4a3d431f6b6b
wlals3665 <85783000+wlals3665@users.noreply.github.com>
2021-09-28 15:40:58 +0900

    Update assign0928_20213001.py

--- assignment/assign0928/sumofeachnum/assign0928_20213001.py
+++ assignment/assign0928/sumofeachnum/assign0928_20213001.py
@@ -1,13 +1,4 @@
 #박지민 -20213001
-def solution(num) :
-  answer = 0
-  s = set(num)
-
-  if len(num)==len(s) :
-      print("자주나타나는 숫자는없다")
-  elif len(num)==1 :
-      print("없다")
-  else :
-      dic={i:num.count(i) for i in s}
-
-  answer = max(dic, key=dic.get)
+def solution(num):
+  answer = sum([int(i) for i in str(num)])
+  return answer
