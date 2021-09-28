ffc6ef38ae62d3a3671b1daed282cb1f260d2da9
tldbs0420 <84086266+tldbs0420@users.noreply.github.com>
2021-09-28 15:16:25 +0900

    [Add] Added assgin0928-20212997.py Files
    /assignment
        +assign0928
            +maxfreqnum
                + assign0928-20212997.py
            +sumofeachnum
                + assign0928-20212997.py

---
+++ assignment/assign0928/sumofeachnum/assign0928-20212997.py
@@ -0,0 +1,9 @@
+def solution(num):
+    list = [int(i) for i in str(num)]
+    answer=sum(list)
+    return answer
+
+print(solution(5923)) #19
+print(solution(200)) # 2
+print(solution(1234567890)) #45
+print(solution(2364759387)) #54
