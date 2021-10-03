4956ee694f3a2009f7c5d4265dbabcefbf981754
JW0705 <jwood-@naver.com>
2021-10-03 19:50:50 +0900

    [MODIFY] Modified some codes

--- assignment/assign0928/sumofeachnum/assign0928_20212999.py
+++ assignment/assign0928/sumofeachnum/assign0928_20212999.py
@@ -1,10 +1,11 @@
 # 박재우-20212999
 
 def solution(num):
-    answer = 0
-    while(num!=0):
-        answer += num%10
-        num = num//10
+    # answer = 0
+    # while(num!=0):
+    #   answer += num%10
+    #   num = num//10
+    answer = sum([int(i) for i in str(num)])
     return answer
 
 print(solution(5923)) #19
