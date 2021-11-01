6997edde1827dee69fca6587a4636daa84d366ec
Park Jonghyeok <84115021+toddla23@users.noreply.github.com>
2021-11-01 16:39:05 +0900

    Update assign1102_20213000.py

--- assignment/assign1102/carculator/assign1102_20213000.py
+++ assignment/assign1102/carculator/assign1102_20213000.py
@@ -7,17 +7,17 @@ from PyQt5.QtWidgets import QLayout, QGridLayout
 
 class Button(QToolButton):
 
-    def __init__(self, text):
-        super().__init__()
+    def __init__(self, text, callback):
+        super().__init__(self, text, callback)
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
         self.setText(text)
+        self.clicked.connect(callback)
 
     def sizeHint(self):
         size = super(Button, self).sizeHint()
         size.setHeight(size.height() + 20)
         size.setWidth(max(size.width(), size.height()))
         return size
-
 
 class Calculator(QWidget):
 
@@ -30,19 +30,21 @@ class Calculator(QWidget):
         self.display.setAlignment(Qt.AlignRight)
         self.display.setMaxLength(15)
 
+
+
         # Digit Buttons
         self.digitButton = [x for x in range(0, 10)]
 
-        self.digitButton[0] = Button('0')
-        self.digitButton[1] = Button('1')
-        self.digitButton[2] = Button('2')
-        self.digitButton[3] = Button('3')
-        self.digitButton[4] = Button('4')
-        self.digitButton[5] = Button('5')
-        self.digitButton[6] = Button('6')
-        self.digitButton[7] = Button('7')
-        self.digitButton[8] = Button('8')
-        self.digitButton[9] = Button('9')
+        self.digitButton[0] = Button('0', self.buttonClicked)
+        self.digitButton[1] = Button('1', self.buttonClicked)
+        self.digitButton[2] = Button('2', self.buttonClicked)
+        self.digitButton[3] = Button('3', self.buttonClicked)
+        self.digitButton[4] = Button('4', self.buttonClicked)
+        self.digitButton[5] = Button('5', self.buttonClicked)
+        self.digitButton[6] = Button('6', self.buttonClicked)
+        self.digitButton[7] = Button('7', self.buttonClicked)
+        self.digitButton[8] = Button('8', self.buttonClicked)
+        self.digitButton[9] = Button('9', self.buttonClicked)
 
         # . and = Buttons
         self.decButton = Button('.')
@@ -103,6 +105,16 @@ class Calculator(QWidget):
 
         self.setWindowTitle("My Calculator")
 
+    def buttonClicked(self):
+        button = self.sender()
+        key = button.text()
+        if key == "=":
+            result = str(eval(self.display.text()))
+            self.display.setText(result)
+        elif key == "C":
+            self.display.setText('')
+        else:
+            self.display.setText(self.dispaly.text() + key)
 
 if __name__ == '__main__':
 
